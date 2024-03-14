fb2eeea0644a6046d099f1ae01635fbcd7613a22
K-Honsu <feranmia51@gmail.com>
2024-03-15 00:41:01 +0100

    feat:add swagger file

--- src/utils/swagger/swagger.js
+++ src/utils/swagger/swagger.js
@@ -4,7 +4,7 @@ var doc = {
         title: 'Clutter - Cut the Clutter, Share the Flair',
         description: 'API documentation of all endpoints.',
     },
-    host: 'https://scissors-kl37.onrender.com',
+    host: 'scissors-kl37.onrender.com',
     schemes: ['http'],
 };
 var outputFile = '../../swagger-output.json';
