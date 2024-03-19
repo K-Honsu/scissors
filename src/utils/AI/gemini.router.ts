9caa4785b3b395be12f42346ebcc3014cb0d6ddb
K-Honsu <feranmia51@gmail.com>
2024-03-19 18:24:14 +0100

    feat:add route to chat

--- src/utils/AI/gemini.router.ts
+++ src/utils/AI/gemini.router.ts
@@ -5,6 +5,6 @@ import { Router } from "express";
 const geminiRouter = Router({ mergeParams: true })
 
 // userRouter.post("/", userMiddleware, createUser)
-geminiRouter.route("/").get(BearerToken ,generateResponse)
+geminiRouter.route("/chat").get(BearerToken ,generateResponse)
 
 export default geminiRouter
